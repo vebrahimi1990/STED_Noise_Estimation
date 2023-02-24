e4da10b951b7f4adc70eca6f1a18ebb66f00154a
Vahid Ebrahimi <95882710+vebrahimi1990@users.noreply.github.com>
2023-02-24 13:36:32 -0500

    Initial commit

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 CFG = {
-    'noisy_dr': r'D:\Projects\STED_Noise_Estimation\image_files\noisy.tif',
-    'highSNR_dr': r'D:\Projects\STED_Noise_Estimation\image_files\gt.tif',
+    'noisy_dr': r'\image_files\noisy.tif',
+    'highSNR_dr': r'\image_files\gt.tif',
     'num_bins': 100,
     'hist_range': [0.01, 1],
     'lamb_poisson_range': [0.05, 0.7],
